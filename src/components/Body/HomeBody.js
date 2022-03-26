6a36c7047ad3d773619d0df79fa45c62a97c502c
Charlie Welch <welch.charlie16@gmail.com>
2022-03-26 16:54:25 -0500

    Fixing some styling on the home page

--- src/components/Body/HomeBody.js
+++ src/components/Body/HomeBody.js
@@ -1,58 +1,56 @@
-import '../../App.css';
-import { homeInfo } from '../../data/data';
+import "../../App.css";
+import { FaInfoCircle } from "react-icons/fa";
+import { homeInfo } from "../../data/data";
 
 const HomeBody = () => {
-	return (
-		<main>
-			{homeInfo.map((item, idx) => (
-				<div
-					className="bg-gray-darker  pt-5 md:pb-5 flex justify-center"
-					id="body"
-					key={idx}
-				>
-					<div className="bg-gray-lighter w-4/5  pt-5 md:pb-10 rounded z-20 border-4 border-gray-darkest">
-						<h1 className="text-gray-darkest text-center text-7xl">
-							{item.header}
-						</h1>
-						<div className="md:pl-16 pt-10 md:flex">
-							<img
-								src={item.img}
-								alt="Tshe Nelson"
-								className="h-30 md:h-60 rounded-full"
-							></img>
-							<p className="px-5 text-2xl leading-loose text-gray-darkest">
-								{item.paragraph}
-							</p>
-						</div>
-						<div className="md:flex">
-							<ul className="list-disc md:ml-16 mt-10 text-2xl md:mr-5 md:px-4 list-inside text-gray-darkest rounded-lg border-4 border-gray-darkest bg-gray-lightest shadow-xl">
-								<div className="font-semibold">
-									{item.skillsTitle}
-								</div>
-								{item.skills.map(({ value }, idx) => (
-									<li className="text-xl" key={idx}>
-										{value}
-									</li>
-								))}
-							</ul>
-							<div className="md:mt-10 text-gray-darkest md:pr-2 md:mr-5 border-4 border-gray-darkest pl-4 rounded-lg bg-gray-lightest shadow-xl">
-								<p className="text-2xl font-semibold">
-									{item.recommendationTitle}
-								</p>
-								<p className="text-lg">
-									{' '}
-									{item.recommendation}
-								</p>
-								<p className="pt-2 md:float-right">
-									{item.job}
-								</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			))}
-		</main>
-	);
+  return (
+    <main>
+      {homeInfo.map((item, idx) => (
+        <div
+          className="bg-gray-darker  pt-5 md:pb-5 flex justify-center"
+          id="body"
+          key={idx}
+        >
+          <div className="bg-gray-lighter w-4/5  pt-5 md:pb-10 rounded z-20 border-4 border-gray-darkest">
+            <h1 className="text-gray-darkest text-center text-7xl">
+              {item.header}
+            </h1>
+            <div className="md:pl-16 pt-10 md:flex">
+              <img
+                src={item.img}
+                alt="The Nelson"
+                className="h-30 md:h-60 rounded-full"
+              ></img>
+              <p className="px-5 text-2xl leading-loose text-gray-darkest">
+                {item.paragraph}
+              </p>
+            </div>
+            <div className="lg:flex m-4 gap-4">
+              <ul className="px-2 mb-4 lg:mb-0 whitespace-nowrap list-disc list-inside text-gray-darkest rounded-lg border-4 border-gray-darkest bg-gray-lightest shadow-xl">
+                <div className="font-semibold text-2xl">{item.skillsTitle}</div>
+                {item.skills.map(({ value }, idx) => (
+                  <li className="text-lg" key={idx}>
+                    {value}
+                  </li>
+                ))}
+                <div className="py-2 italic flex items-center">
+                  <FaInfoCircle className="h-6 w-6" />
+                  <p className="px-2 text-lg">{item.preferredStack}</p>
+                </div>
+              </ul>
+              <div className="px-2 text-gray-darkest border-4 border-gray-darkest rounded-lg bg-gray-lightest shadow-xl">
+                <p className="text-2xl font-semibold">
+                  {item.recommendationTitle}
+                </p>
+                <p className="text-lg leading-loose"> {item.recommendation}</p>
+                <p className="pt-2 flex justify-end">{item.job}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </main>
+  );
 };
 
 export default HomeBody;
--- src/data/data.js
+++ src/data/data.js
@@ -1,279 +1,288 @@
-import Nelson from '../images/IMG_2260.JPG'
-import Portfolio from '../images/Portfolio.png'
-import Sports from '../images/Sports-Social-Media.png'
-import Lawn from '../images/4th-and-lawn.png'
-import FeedbackUI from '../images/FeedbackUI.png'
+import Nelson from "../images/IMG_2260.JPG";
+import Portfolio from "../images/Portfolio.png";
+import Sports from "../images/Sports-Social-Media.png";
+import Lawn from "../images/4th-and-lawn.png";
+import FeedbackUI from "../images/FeedbackUI.png";
 
 export const homeInfo = [
   {
     img: Nelson,
-    header: 'Hello, World!',
+    header: "Hello, World!",
     paragraph:
       "I am a young, full-stack developer with experience in assembly language programming and object oriented design. I'm currently in my final semester as a computer science major and have studied a broad spectrum of information systems, with an emphasis on data structures and algorithm analysis for front and back end development.",
-    skillsTitle: 'Certified skills: ',
+    skillsTitle: "Certified skills: ",
+    preferredStack: " My preferred stack is MERN",
     skills: [
       {
-        value: 'HTML',
+        value: "HTML",
       },
       {
-        value: 'CSS',
+        value: "CSS",
       },
       {
-        value: 'JavaScript',
+        value: "JavaScript",
       },
       {
-        value: 'PHP',
+        value: "JQuery",
       },
       {
-        value: 'MySQL',
+        value: "PHP",
       },
       {
-        value: 'C++',
+        value: "MySQL",
       },
       {
-        value: 'ReactJS',
+        value: "C++",
+      },
+      {
+        value: "ReactJS",
+      },
+      {
+        value: "MongoDB",
+      },
+      {
+        value: "Express",
+      },
+      {
+        value: "NodeJS",
       },
     ],
-    recommendationTitle: 'Received Recommendations:',
+    recommendationTitle: "Received Recommendations:",
     recommendation:
       "I worked with Charles on multiple projects during our time in Bootcamp. He is a quick study and a willing teacher. Many times, I would reach out to him for an explanation of concepts and review of my code. During our projects, he demonstrated problem-solving and a team-first approach. For one project we collaborated to complete a full backend ourselves. Another project he took on the challenge of working multiple external APIs into the project seamlessly. From my experience collaborating with Charles, he has a strong grasp on full stack development concepts and ;has shown the ability to be an excellent communicator. When the 'going got touch' during our projects, he dug in and hit deadlines. I've enjoyed my time collaborating with Charles and would work with him on future projects.",
-    job: '-Senior Sales Engineer at AppDynamics',
+    job: "-Senior Sales Engineer at AppDynamics",
   },
-]
+];
 
 export const contactInfo = [
   {
-    header: 'Contact Me',
+    header: "Contact Me",
     formInputs: [
       {
-        type: 'text',
-        name: 'firstname',
-        id: 'fname',
+        type: "text",
+        name: "firstname",
+        id: "fname",
         required: true,
-        text: 'First Name: ',
+        text: "First Name: ",
       },
       {
-        type: 'text',
-        name: 'lastname',
-        id: 'lname',
+        type: "text",
+        name: "lastname",
+        id: "lname",
         required: true,
-        text: 'Last Name: ',
+        text: "Last Name: ",
       },
       {
-        type: 'tel',
-        name: 'phone',
-        id: 'phone',
-        placeholder: 'Format: 123-456-7890',
-        pattern: '[0-9]{3}-[0-9]{3}-[0-9]{4}',
-        text: 'Phone: ',
+        type: "tel",
+        name: "phone",
+        id: "phone",
+        placeholder: "Format: 123-456-7890",
+        pattern: "[0-9]{3}-[0-9]{3}-[0-9]{4}",
+        text: "Phone: ",
         required: false,
       },
       {
-        type: 'email',
-        name: 'email',
-        id: 'email',
+        type: "email",
+        name: "email",
+        id: "email",
         required: true,
-        text: 'Email: ',
+        text: "Email: ",
       },
       {
-        type: 'textarea',
-        name: 'message',
-        id: 'messge',
+        type: "textarea",
+        name: "message",
+        id: "messge",
         required: true,
-        text: 'Message: ',
+        text: "Message: ",
       },
     ],
   },
-]
+];
 
 export const projectInfo = [
   {
-    title: 'Portfolio Website:',
+    title: "Portfolio Website:",
     img: Portfolio,
-    link: 'https://github.com/cmattrox/Portfolio',
-    text:
-      'This is my portfolio website, built with React and Tailwindcss. I built this platform to introduce myself, show you what I have done, and give you a place to connect with me. Everything you see here was designed by me, even this carousel! Links under my projects will take you to the respective github pages and from there you can take a look at all of my repositories to see what I have worked on over the course of my career in development. The Contact Me page uses email.js, so if you would like to know more about any of my projects, or set up a meeting, please feel free to fill out the forum!',
+    link: "https://github.com/cmattrox/Portfolio",
+    text: "This is my portfolio website, built with React and Tailwindcss. I built this platform to introduce myself, show you what I have done, and give you a place to connect with me. Everything you see here was designed by me, even this carousel! Links under my projects will take you to the respective github pages and from there you can take a look at all of my repositories to see what I have worked on over the course of my career in development. The Contact Me page uses email.js, so if you would like to know more about any of my projects, or set up a meeting, please feel free to fill out the forum!",
     technologies: [
       {
-        title: 'React',
+        title: "React",
       },
       {
-        title: 'TailwindCSS',
+        title: "TailwindCSS",
       },
       {
-        title: 'NPM',
+        title: "NPM",
       },
       {
-        title: 'JQuery',
+        title: "JQuery",
       },
       {
-        title: 'Email.JS',
+        title: "Email.JS",
       },
       {
-        title: 'FontAwesome',
+        title: "FontAwesome",
       },
       {
-        title: 'Github Pages',
+        title: "Github Pages",
       },
       {
-        title: 'PostCSS',
+        title: "PostCSS",
       },
       {
-        title: 'Autoprefixer',
+        title: "Autoprefixer",
       },
       {
-        title: 'Craco',
+        title: "Craco",
       },
     ],
   },
   {
-    title: '4th-and-lawn',
+    title: "4th-and-lawn",
     img: Lawn,
-    link: 'https://github.com/cmattrox/4th-and-lawn',
-    text:
-      '4th-and-Lawn is an application I developed with several of my peers that we designed for users to be able to rent out parking spaces for events and trips to ensure you have a spot once you arrive. My main role consisted of implementing external APIs, such as Google Maps, in back-end development. I even set up an automatic confirmation email to add to the user’s experience.',
+    link: "https://github.com/cmattrox/4th-and-lawn",
+    text: "4th-and-Lawn is an application I developed with several of my peers that we designed for users to be able to rent out parking spaces for events and trips to ensure you have a spot once you arrive. My main role consisted of implementing external APIs, such as Google Maps, in back-end development. I even set up an automatic confirmation email to add to the user’s experience.",
     technologies: [
       {
-        title: 'React',
+        title: "React",
       },
       {
-        title: 'Heroku',
+        title: "Heroku",
       },
       {
-        title: 'NPM',
+        title: "NPM",
       },
       {
-        title: 'Nodemon',
+        title: "Nodemon",
       },
       {
-        title: 'Yarn',
+        title: "Yarn",
       },
       {
-        title: 'Node.JS',
+        title: "Node.JS",
       },
       {
-        title: 'MongoDB',
+        title: "MongoDB",
       },
       {
-        title: 'Express',
+        title: "Express",
       },
       {
-        title: 'Mongoose',
+        title: "Mongoose",
       },
       {
-        title: 'Google Maps API',
+        title: "Google Maps API",
       },
       {
-        title: 'SASS',
+        title: "SASS",
       },
       {
-        title: 'NodeMailer',
+        title: "NodeMailer",
       },
       {
-        title: 'Passport',
+        title: "Passport",
       },
       {
-        title: 'Axios',
+        title: "Axios",
       },
       {
-        title: 'Animate.CSS',
+        title: "Animate.CSS",
       },
       {
-        title: 'JQuery',
+        title: "JQuery",
       },
     ],
   },
   {
-    title: 'Sports Social Media App',
+    title: "Sports Social Media App",
     img: Sports,
-    link: 'https://github.com/cmattrox/project-2',
-    text:
-      'This was a group project designed to enable users to post and find times, dates, and locations for recreational pick-up games. This application has many functions, including the ability for users to create players, keep track of their games, and view other’s activities. We used Handlebars.js to build out the templates we needed, and Express.js as the backend server framework. ',
+    link: "https://github.com/cmattrox/project-2",
+    text: "This was a group project designed to enable users to post and find times, dates, and locations for recreational pick-up games. This application has many functions, including the ability for users to create players, keep track of their games, and view other’s activities. We used Handlebars.js to build out the templates we needed, and Express.js as the backend server framework. ",
     technologies: [
       {
-        title: 'Express',
+        title: "Express",
       },
       {
-        title: 'SASS',
+        title: "SASS",
       },
       {
-        title: 'Handlebars',
+        title: "Handlebars",
       },
       {
-        title: 'MySQL',
+        title: "MySQL",
       },
       {
-        title: 'Node.JS',
+        title: "Node.JS",
       },
       {
-        title: 'Passport',
+        title: "Passport",
       },
       {
-        title: 'Chai',
+        title: "Chai",
       },
       {
-        title: 'Sequelize',
+        title: "Sequelize",
       },
       {
-        title: 'Mocha',
+        title: "Mocha",
       },
     ],
   },
   {
-    title: 'Feedback UI',
+    title: "Feedback UI",
     img: FeedbackUI,
-    link: 'https://github.com/cmattrox/feedback-application',
-    text:
-      'This is an application from udemy course React Front to Back 2022. It is a simple application that allows you to leave feedback on a service, with a ranking and review. The app gives a total number of reviews and an average rating as well as shows all past reviews. The user can also edit and delete past reviews on top of adding new reviews. I did add a few changes of my own from the course. I implemented tailwindcss and daisyui to consolidate styling and changed a bunch of the stylings myself.',
+    link: "https://github.com/cmattrox/feedback-application",
+    text: "This is an application from udemy course React Front to Back 2022. It is a simple application that allows you to leave feedback on a service, with a ranking and review. The app gives a total number of reviews and an average rating as well as shows all past reviews. The user can also edit and delete past reviews on top of adding new reviews. I did add a few changes of my own from the course. I implemented tailwindcss and daisyui to consolidate styling and changed a bunch of the stylings myself.",
     technologies: [
       {
-        title: 'ReactJS',
+        title: "ReactJS",
       },
       {
-        title: 'TailwindCSS',
+        title: "TailwindCSS",
       },
       {
-        title: 'DaisyUI',
+        title: "DaisyUI",
       },
       {
-        title: 'JSON Server',
+        title: "JSON Server",
       },
       {
-        title: 'Concurrently',
+        title: "Concurrently",
       },
       {
-        title: 'PostCSS',
+        title: "PostCSS",
       },
       {
-        title: 'Autoprefixer',
+        title: "Autoprefixer",
       },
     ],
   },
-]
+];
 
 export const experienceInfo = [
   {
-    header: 'Experience',
+    header: "Experience",
     experience: [
       {
-        title: 'International Academy of Science (Full-Stack Developer):',
-        time: 'September 2021 - Current',
+        title: "International Academy of Science (Full-Stack Developer):",
+        time: "September 2021 - Current",
         description:
           "The International Academy of Science is an online, post-secondary, educational platform. on the web development team with five peers to collaborate and develop a clean, efficient, design for development projects in our que. I specialized in connecting front and back-end sources to generate essential data for pages while maintaining high standards of the industry. I primarily used HTML, CSS, JavaScript, React, PHP, and MySQL. Additionally, I facilitated a transition to React based coding to maintain the consistency of the company's websites while ensuring current projects are up-to-date and effective. ",
       },
       {
         title:
           "Metropolitan Community College (Associate's in Computer Science):",
-        time: 'January 2020 - May 2022',
+        time: "January 2020 - May 2022",
         description:
-          'I am currently enrolled in a broad spectrum of computer science class as Metropolitan Community College. After graduating with my associate’s degree, I plan on transferring to the University of Missouri--Kansas City to pursue my bachelor’s degree in computer science. Throughout my education, my main focus has been on data structures and algorithm analysis, object oriented design, and assembly language programming. ',
+          "I am currently enrolled in a broad spectrum of computer science class as Metropolitan Community College. After graduating with my associate’s degree, I plan on transferring to the University of Missouri--Kansas City to pursue my bachelor’s degree in computer science. Throughout my education, my main focus has been on data structures and algorithm analysis, object oriented design, and assembly language programming. ",
       },
       {
-        title: 'University of Kansas (Full-Stack Bootcamp):',
-        time: 'May 2018 - October 2018',
+        title: "University of Kansas (Full-Stack Bootcamp):",
+        time: "May 2018 - October 2018",
         description:
-          'During my time at Kansas University, I was part of a full-stack web development program administered by KU and Trilogy. Here, I honed my skills using HTML, CSS, and JavaScript, as well as using front-end libraries, like React. Additionally, I worked with my peers to develop several applications, such as a real estate home locator. The highlights of this bootcamp included building and maintaining MongoDB and MySQL databases, along with integrating APIs into projects for access to different databases and tools. ',
+          "During my time at Kansas University, I was part of a full-stack web development program administered by KU and Trilogy. Here, I honed my skills using HTML, CSS, and JavaScript, as well as using front-end libraries, like React. Additionally, I worked with my peers to develop several applications, such as a real estate home locator. The highlights of this bootcamp included building and maintaining MongoDB and MySQL databases, along with integrating APIs into projects for access to different databases and tools. ",
       },
     ],
   },
-]
+];
