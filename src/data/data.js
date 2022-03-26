01049135cacee626498c9eae5278fadd3d42d2de
Charlie Welch <welch.charlie16@gmail.com>
2022-03-26 17:13:04 -0500

    Fixing spelling errors

--- src/data/data.js
+++ src/data/data.js
@@ -49,7 +49,7 @@ export const homeInfo = [
     ],
     recommendationTitle: "Received Recommendations:",
     recommendation:
-      "I worked with Charles on multiple projects during our time in Bootcamp. He is a quick study and a willing teacher. Many times, I would reach out to him for an explanation of concepts and review of my code. During our projects, he demonstrated problem-solving and a team-first approach. For one project we collaborated to complete a full backend ourselves. Another project he took on the challenge of working multiple external APIs into the project seamlessly. From my experience collaborating with Charles, he has a strong grasp on full stack development concepts and ;has shown the ability to be an excellent communicator. When the 'going got touch' during our projects, he dug in and hit deadlines. I've enjoyed my time collaborating with Charles and would work with him on future projects.",
+      "I worked with Charles on multiple projects during our time in Bootcamp. He is a quick study and a willing teacher. Many times, I would reach out to him for an explanation of concepts and review of my code. During our projects, he demonstrated problem-solving and a team-first approach. For one project we collaborated to complete a full backend ourselves. Another project he took on the challenge of working multiple external APIs into the project seamlessly. From my experience collaborating with Charles, he has a strong grasp on full stack development concepts and has shown the ability to be an excellent communicator. When the 'going got tough' during our projects, he dug in and hit deadlines. I've enjoyed my time collaborating with Charles and would work with him on future projects.",
     job: "-Senior Sales Engineer at AppDynamics",
   },
 ];
